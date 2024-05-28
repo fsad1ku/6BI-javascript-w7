ee50164b0790dd65c30a8f50bc4a275a429ea58d
fsad1ku <167757968+fsad1ku@users.noreply.github.com>
2024-05-28 10:06:18 +0000

    28/05/24

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//paragraaf
+console.log(document.getElementById("para").innerText);
+//image
+console.log(document.getElementById("image").src);
+//link 
+console.log(document.getElementById("hyperlink").href);
